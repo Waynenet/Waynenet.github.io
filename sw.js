a4c369332cea06908b24ee82d7f9e0991142a94b
Wayne <waynenet8@gmail.com>
2022-06-29 00:03:33 +0800

    Site updated: 2022-06-29 00:03:27

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"21a7517227c2c3d7275a60a57518b660","url":"./404.html"},{"revision":"baa0d0f9605aa97827e5520b72328e0d","url":"./index.html"},{"revision":"d88b63a0b350b4838f3398a38f0520e8","url":"./js/main.js"},{"revision":"9413b1df55a33a053ce5ee7d197a0596","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"e6043411d258b7094f5442b358346a93","url":"./404.html"},{"revision":"f4d0bf7a9958e84af9498ded20c6e6eb","url":"./index.html"},{"revision":"d88b63a0b350b4838f3398a38f0520e8","url":"./js/main.js"},{"revision":"9413b1df55a33a053ce5ee7d197a0596","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
