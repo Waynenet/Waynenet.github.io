92751bbe00e46a9da0223d9720b1bb04c15a37b5
Wayne <way8941@gmail.com>
2025-07-09 07:47:54 +0000

    5.4.2.0 更新主题5.4.2，修复bug UTC 2025-07-09 Wednesday 07:47:54 Updated By Github Actions

---
+++ sw.js
@@ -0,0 +1,96 @@
+const workboxVersion = '7.3.0';
+
+importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
+
+workbox.core.setCacheNameDetails({
+    prefix: "wayne"
+});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+// 注册成功后要立即缓存的资源列表
+// 具体缓存列表在gulpfile.js中配置，见下文
+workbox.precaching.precacheAndRoute([{"revision":"ad674d7d2f6e5c350203d2dee54a182e","url":"./404.html"},{"revision":"55f71ca6cca8982556b0524bec74a2a8","url":"./index.html"},{"revision":"bfe5402f5a932a03ab22f04c38b82fba","url":"./js/main.js"},{"revision":"06a4cc414171a375e8b7abfd51b7a325","url":"./css/index.css"}],{
+    directoryIndex: null
+});
+
+// 清空过期缓存
+workbox.precaching.cleanupOutdatedCaches();
+
+// 图片资源（可选，不需要就注释掉）
+workbox.routing.registerRoute(
+    /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "images",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// 字体文件（可选，不需要就注释掉）
+workbox.routing.registerRoute(
+    /\.(?:eot|ttf|woff|woff2)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "fonts",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// 谷歌字体（可选，不需要就注释掉）
+//workbox.routing.registerRoute(
+//    /^https:\/\/fonts\.googleapis\.com/,
+//    new workbox.strategies.StaleWhileRevalidate({
+//        cacheName: "google-fonts-stylesheets"
+//    })
+//);
+//workbox.routing.registerRoute(
+//    /^https:\/\/fonts\.gstatic\.com/,
+//    new workbox.strategies.CacheFirst({
+//        cacheName: 'google-fonts-webfonts',
+//        plugins: [
+//            new workbox.expiration.ExpirationPlugin({
+//                maxEntries: 1000,
+//                maxAgeSeconds: 60 * 60 * 24 * 30
+//            }),
+//            new workbox.cacheableResponse.CacheableResponsePlugin({
+//                statuses: [0, 200]
+//            })
+//        ]
+//    })
+//);
+
+// jsdelivr的CDN资源（可选，不需要就注释掉）
+//workbox.routing.registerRoute(
+//    /^https:\/\/cdn\.jsdelivr\.net/,
+//    new workbox.strategies.CacheFirst({
+//        cacheName: "static-libs",
+//        plugins: [
+//            new workbox.expiration.ExpirationPlugin({
+//                maxEntries: 1000,
+//                maxAgeSeconds: 60 * 60 * 24 * 30
+//            }),
+//            new workbox.cacheableResponse.CacheableResponsePlugin({
+//                statuses: [0, 200]
+//            })
+//        ]
+//    })
+//);
+
+workbox.googleAnalytics.initialize();
