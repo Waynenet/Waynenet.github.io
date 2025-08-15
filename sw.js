db6d225a49a612c5bc794730332732c2641416d8
Waynenet <54445994+Waynenet@users.noreply.github.com>
2025-08-15 09:00:18 +0000

    Deploy: chore: 升级字体文件

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"cbb868590f1345b7605b0897a526a0fa","url":"./404.html"},{"revision":"ed214cf30a327a4d7085172b64773c85","url":"./index.html"},{"revision":"bfe5402f5a932a03ab22f04c38b82fba","url":"./js/main.js"},{"revision":"fb7746121e90101e55ba7ba22fc3ded6","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"cbb868590f1345b7605b0897a526a0fa","url":"./404.html"},{"revision":"c281c63ff9bda3346ab5428327a3d0fc","url":"./index.html"},{"revision":"bfe5402f5a932a03ab22f04c38b82fba","url":"./js/main.js"},{"revision":"fb7746121e90101e55ba7ba22fc3ded6","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
