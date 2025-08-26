81334b3b78339a9fd9963048e4f638d54ee5f0ce
Waynenet <54445994+Waynenet@users.noreply.github.com>
2025-08-26 06:02:54 +0000

    refactor: 重构gulp

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"cbb868590f1345b7605b0897a526a0fa","url":"./404.html"},{"revision":"b339be7d3afff2a0dc2052dba4b409cb","url":"./index.html"},{"revision":"bfe5402f5a932a03ab22f04c38b82fba","url":"./js/main.js"},{"revision":"fb7746121e90101e55ba7ba22fc3ded6","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"cbb868590f1345b7605b0897a526a0fa","url":"./404.html"},{"revision":"37dd871a65189d25df618794318fd0ce","url":"./index.html"},{"revision":"bfe5402f5a932a03ab22f04c38b82fba","url":"./js/main.js"},{"revision":"fb7746121e90101e55ba7ba22fc3ded6","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
